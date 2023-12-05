38fc04ef4fc2526d0f1c28546d55c544c0c4a736
jfrasinska <j.frasinska@gmail.com>
2023-12-05 22:47:54 +0100

    refactoring with bug fixing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ContactForm from '../ContactFrom/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import './App.css';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleInputChange = e => {
+  const handleInputChange = e => {
     const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    const { name, number, contacts } = this.state;
 
     if (name.trim() === '' || number.trim() === '') {
       alert('Please enter a valid name and number.');
@@ -48,60 +46,51 @@ class App extends Component {
       number: number.trim(),
     };
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
-    }));
+    setContacts(prevContacts => [...prevContacts, newContact]);
+    setName('');
+    setNumber('');
   };
 
-  handleFilterChange = e => {
+  const handleFilterChange = e => {
     const { value } = e.target;
-    this.setState({
-      filter: value,
-    });
+    setFilter(value);
   };
 
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
 
-  render() {
-    const { name, number, filter, contacts } = this.state;
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    const filteredContacts = contacts.filter(
-      contact =>
-        contact.name &&
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <div className="wrapper">
-        <h1>Contact Book App</h1>
-        <ContactForm
-          name={name}
-          number={number}
-          handleInputChange={this.handleInputChange}
-          handleSubmit={this.handleSubmit}
+  return (
+    <div className="wrapper">
+      <h1>Contact Book App</h1>
+      <ContactForm
+        name={name}
+        number={number}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
+      <label className="filter">
+        Filter by name:
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={handleFilterChange}
         />
-        <label className="filter">
-          Filter by name:
-          <input
-            type="text"
-            name="filter"
-            value={filter}
-            onChange={this.handleFilterChange}
-          />
-        </label>
-        <ContactList
-          contacts={filteredContacts}
-          onDeleteContact={this.handleDeleteContact}
-        />
-      </div>
-    );
-  }
-}
+      </label>
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
 
 export default App;
